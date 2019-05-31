50a7cd0a5cb309cc1f474b06f71d338dda9d1cd0
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-05-31 13:40:28 +0900

    Add seeds for Task model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!(
+    title: "1st task",
+    description: "This is the first task, yay!"
+)
+Task.create!(
+    title: "2nd task",
+    description: "This is the 2nd task, I suppose."
+)
+Task.create!(
+    title: "3rd task",
+    description: "Yes, this is the 3rd task."
+)
+Task.create!(
+    title: "Do something important",
+    description: "You need to finish something very important, hurry up!"
+)
