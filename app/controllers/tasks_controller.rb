8ba60f08740ed7710803f879f497e60dbdda584c
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-05-31 11:37:01 +0900

    Add function to create new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,8 +7,25 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_url, notice: "タスクを登録しました。"
+    else
+      flash.now.alert = "タスクを登録できません。"
+      render :new
+    end
   end
 
   def edit
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
